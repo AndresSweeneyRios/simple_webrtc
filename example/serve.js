fa4da6dfa7917cfcea76158d6d44e187b531bceb
Andr3wRiv3rs <andrewarivers@gmail.com>
2023-06-18 13:32:50 -0700

    3.0

--- example/serve.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use('/scripts', express.static(__dirname.replace('example','src')))
-app.use('/', express.static(__dirname))
-
-app.listen(4080)
-
-console.log(`Listening at http://localhost:4080`)
